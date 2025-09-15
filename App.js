2d778e81ffeb8ef100b2168ea951d0a5c5cbd90a
JubelleF <jubellefranze1907@gmail.com>
2025-09-15 13:07:10 +0800

    App.js modified

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import ChatScreen from './ChatScreen';
 
-import React from "react";
-import { SafeAreaView, StyleSheet, Text} from "react-native";
-import ChatScreen from "./ChatScreen";
-
-export default function App() {
+const SafeAreaComponent = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ChatScreen />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1 ,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-  text : {
-    color: 'hsla(0,79.7%,65.3%,0.808)',
-  }
 });
+
+export default SafeAreaComponent;
